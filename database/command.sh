c81f3a21c22b10522ad3cc2f000925e9e3bee592
satish <karasatishkumar@gmail.com>
2013-03-03 21:33:49 +0530

    SSO configuration for all fourservices.

--- database/command.sh
+++ database/command.sh
@@ -1,10 +1,10 @@
 ##Web Server based app request##
-http://localhost:8080/oauth2/oauth/authorize?response_type=code&client_id=easylocate&scope=read&redirect_uri=http://localhost:8080/web
+http://localhost:8080/geodataservice/oauth/authorize?response_type=code&client_id=easylocate&scope=read&redirect_uri=http://localhost:8080/web
 
-http://localhost:8080/oauth2/oauth/token?grant_type=authorization_code&code=H7Dfm3&redirect_uri=http://localhost:8080/roleservice/role&client_id=easylocate&client_secret=secret
+http://localhost:8080/geodataservice/oauth/token?grant_type=authorization_code&code=H7Dfm3&redirect_uri=http://localhost:8080/roleservice/api/role&client_id=easylocate&client_secret=secret
 
 ##Browser Based request#
-http://localhost:8080/oauth2/oauth/authorize?response_type=token&client_id=easylocate&redirect_uri=http://localhost:8080/web&scope=read
+http://localhost:8080/geodataservice/oauth/authorize?response_type=token&client_id=easylocate&redirect_uri=http://localhost:8080/web&scope=read
 
 curl -i -H "Authorization: Bearer 1f184a6b-cf0f-4767-a6ad-4ba7bee7a8c2" http://localhost:8080/roleservice/api/role
 
